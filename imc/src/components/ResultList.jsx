bf6e43896e4e713eaade6da3ca9641ec5e6a8ff7
ArnaudGaulinDWW <arnogaulin@gmail.com>
2022-08-11 14:57:13 +0200

    gestion des keys et mise en forme

---
+++ imc/src/components/ResultList.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const ResultList = (props) => {
+    console.log(props);
+  return  <li>{props.imc}</li>;
+};
--- imc/src/screens/HomeScreen.jsx
+++ imc/src/screens/HomeScreen.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
-import{ Container, Row, Button, Card } from "react-bootstrap";
+import{ Container, Row, Button, Card, Col } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
-
 
 const interpretations = new Map();
 interpretations.set(16.5, "dénutrition ou anorexie" );
@@ -12,10 +11,11 @@ interpretations.set(35, "obésité modérée");
 interpretations.set(40, "obesité sévère");
 interpretations.set("infinity", "obésité morbide ou massive");
 
-const HomeScreen = () => {
+const erreurs = new Map();
+erreurs.set("pas de nombres négatif")  ;
+erreurs.set(0, "saisir un nombre supérieur a zéro")
 
-let ourImcs = window.localStorage.getItem('imcs') ?
-window.localStorage.getItem('imcs') : []
+const HomeScreen = () => {
 
 let [imcs, setImcs] = useState(
     window.localStorage.getItem('imcs') 
@@ -42,9 +42,7 @@ let handleChange = (evt) =>{
                 theInterpretation = interpretation;
             }
         });
-
-
-        setImc(new Date().toLocaleDateString('fr-FR') + "" + imc + " " + theInterpretation);
+        setImc(new Date().toLocaleDateString('fr-FR') + " " + imc + " " + theInterpretation);
     }
 };
 
@@ -58,52 +56,60 @@ let handleClick = evt => {
     window.localStorage.setItem('imcs', JSON.stringify(newImcs))
 
 };
-
   return (
     <>
     <Card>
         <Container className="container mt-5 mb-5">
             <Row className="row d-flex justify-content-center align-items-center">
-                <Form className="col-lg-6 col-sm-12">
-                    <section className="imc d-flex justify-content-center align-items-center flex-row">
+                <Col>
+                    <Form className="col-lg-6 col-sm-12">
+                        <section className="imc d-flex justify-content-center align-items-center flex-row">
                             <div className=" main_container ">
-                                    <h1 className="text-center">Votre Indice de Masse Corporelle</h1>
-                                <div className="weight_container">
-                                    <Form.Label className="title_card">Votre poids(kg)</Form.Label>
-                                     <input
-                                        onChange={handleChange}
-                                        size="lg"
-                                        id="weight"
-                                        name="weight"
-                                        className="input_card"
-                                        type="number">
-                                     </input>
-                                    
-                                </div>
-                                <div className="height_container">
-                                    <Form.Label className="title_card">Votre taille(cm)</Form.Label>
-                                    <input
-                                        onChange={handleChange}
-                                        size="lg"
-                                        id="height"
-                                        name="height"
-                                        className="input_card"
-                                        type="number">
-                                    </input>
-                                </div>
-                                    <Button onClick={handleClick} className="calculate_button" type="submit">Sauvergarder</Button>
-                                <div id="resultsContainer" className="show_results hidden d-flex justify-content-center align-items-center flex-column">
-                                    <Form.Label>Résultat</Form.Label>
-                                    <div className='interpretation'>
-                                        {imc && <div>{imc}</div>}
+                                        <h1 className="text-center">Votre Indice de Masse Corporelle</h1>
+                                        <br />
+                                    <div className="weight_container">
+                                        <Form.Label className="title_card">Votre poids(kg)</Form.Label>
+                                        <Form.Control
+                                            onChange={handleChange}
+                                            size="lg"
+                                            id="weight"
+                                            name="weight"
+                                            className="input_card"
+                                            type="number">
+                                        </Form.Control>
                                     </div>
-                            <ul>
-                                {imcs.map(i => <li key={i}>{i}</li>)}
-                            </ul>
-                                </div>
+                                    <br />
+                                    <div className="height_container">
+                                        <Form.Label className="title_card">Votre taille(cm)</Form.Label>
+                                        <Form.Control
+                                            onChange={handleChange}
+                                            size="lg"
+                                            id="height"
+                                            name="height"
+                                            className="input_card"
+                                            type="number">
+                                        </Form.Control>
+                                    </div>
+                                    <br />
+                                    <div className='text-center'>
+                                        <Button onClick={handleClick} className="calculate_button" type="submit">Sauvergarder</Button>
+                                    </div>
+                                    <br />
+                                    <div id="resultsContainer" className="show_results hidden d-flex justify-content-center align-items-center flex-column">
+                                        <Form.Label>Résultat</Form.Label>
+                                        <div className='interpretation'>
+                                            {imc && <div>{imc}</div>}
+                                        </div>
+                                    </div>
+                                <ul>
+                                    {imcs.map((i, index) =>(
+                                        <li key={index}>{i} </li>
+                                    ))}
+                                </ul>
                             </div>
-                    </section>
-                </Form>
+                        </section>
+                    </Form>
+                </Col>
             </Row>
         </Container>
     </Card>
